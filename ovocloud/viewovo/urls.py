c0730f6cef796d36116cb08e0292761faf70bcfe
YordanStarting <moncayo0514@gmail.com>
2025-05-15 15:58:49 -0500

    Implementacion de tablas de formulario y creacion de login

--- ovocloud/viewovo/urls.py
+++ ovocloud/viewovo/urls.py
@@ -13,10 +13,13 @@ urlpatterns = [
     path('createpqrs', views.createpqrs, name='createpqrs'),
     path('editpqrs', views.editpqrs, name='editpqrs'),
     # # URL for the complaints wather
-    path('monitereo/', views.listar_monitoreos, name='listar_monitoreos'),
+    path('formularios/monitereo/', views.listar_monitoreos, name='listar_monitoreos'),
     path('nuevo/', views.crear_monitoreo, name='crear_monitoreo'),
     path('editar/<int:pk>/', views.editar_monitoreo, name='editar_monitoreo'),
     path('eliminar/<int:pk>/', views.eliminar_monitoreo, name='eliminar_monitoreo'),
-   path('exportar_pdf/', views.exportar_monitoreo_pdf, name='exportar_pdf'),
+    path('exportar_pdf/', views.exportar_monitoreo_pdf, name='exportar_pdf'),
+    # # URL for the dashboard
+    path('dashboard/', views.dashboard, name='admin_dashboard'),
+    path('formsshow/', views.formsshow, name='formsshow'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ovocloud/viewovo/views.py
+++ ovocloud/viewovo/views.py
@@ -1,9 +1,11 @@
 from django.template.loader import get_template
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.urls import reverse
 from .models import Viewovo, MonitoreoAgua
 from .froms import ovoform, MonitoreoAguaForm
 from xhtml2pdf import pisa
+from django.shortcuts import render
 # Create your views her
 
 def inicio(request):
@@ -12,19 +14,34 @@ def inicio(request):
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
+
+#vista de dashborad
+
+def dashboard(request):
+    return render(request, 'dashboard/index.html')
+
+def formsshow(request):
+    formularios = [
+        {"nombre": "Monitoreo pH","area": "Calidad", "url": reverse('listar_monitoreos')},
+        {"nombre": "Editar PQR","area": "Calidad", "url": reverse('editpqrs')},
+        {"nombre": "Formulario 3","area": "Calidad", "url": reverse('listar_monitoreos')},
+        # Agrega más dinámicamente si quieres
+    ]
+    return render(request, 'directlinks/forms_show.html', {"formularios": formularios})
+
 # complaints views here 
 
 def complaints(request):
     complaints = Viewovo.objects.all()
     print(complaints)
-    return render(request, 'complaints/index.html', {'complaints': complaints})
+    return render(request, 'formularios/complaints/index.html', {'complaints': complaints})
 
 def createpqrs(request):
     formulario = ovoform(request.POST or None)
-    return render(request, 'complaints/crear.html', {'formulario': formulario})
+    return render(request, 'formularios/complaints/crear.html', {'formulario': formulario})
 
 def editpqrs(request):
-    return render(request, 'complaints/editar.html')
+    return render(request, 'formularios/complaints/editar.html')
 
 # complaints aqua here 
 
@@ -37,7 +54,7 @@ def crear_monitoreo(request):
             return redirect('listar_monitoreos')
     else:
         form = MonitoreoAguaForm()
-    return render(request, 'monitoreo/crear.html', {'form': form})
+    return render(request, 'formularios/monitoreo/crear.html', {'form': form})
 
 # LISTAR
 
@@ -50,7 +67,7 @@ def listar_monitoreos(request):
     else:
         monitoreos = MonitoreoAgua.objects.all()
 
-    return render(request, 'monitoreo/listar.html', {
+    return render(request, 'formularios/monitoreo/listar.html', {
         'monitoreos': monitoreos,
         'lote_filtro': lote_filtro,
     })
@@ -65,7 +82,7 @@ def editar_monitoreo(request, pk):
             return redirect('listar_monitoreos')
     else:
         form = MonitoreoAguaForm(instance=monitoreo)
-    return render(request, 'monitoreo/editar.html', {'form': form})
+    return render(request, 'formularios/monitoreo/editar.html', {'form': form})
 
 # ELIMINAR
 def eliminar_monitoreo(request, pk):
@@ -73,12 +90,12 @@ def eliminar_monitoreo(request, pk):
     if request.method == 'POST':
         monitoreo.delete()
         return redirect('listar_monitoreos')
-    return render(request, 'monitoreo/eliminar.html', {'monitoreo': monitoreo})
+    return render(request, 'formularios/monitoreo/eliminar.html', {'monitoreo': monitoreo})
 
 # Descargar Certificado
 def exportar_monitoreo_pdf(request):
     monitoreos = MonitoreoAgua.objects.all()
-    template_path = 'monitoreo/pdf_monitoreos.html'
+    template_path = 'formularios/monitoreo/pdf_monitoreos.html'
     context = {'monitoreos': monitoreos}
     
     response = HttpResponse(content_type='application/pdf')
@@ -92,3 +109,7 @@ def exportar_monitoreo_pdf(request):
     if pisa_status.err:
         return HttpResponse('Error al generar el PDF')
     return response
+
+
+#listar Formularios 
+
